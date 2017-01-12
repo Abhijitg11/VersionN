6fa20511cb40a4a7d57eff5e2faff41da29a954c
Student-004 <Student-004@felix-itss-macbook-pro-2.local>
2017-01-12 17:56:48 +0530

    Initial Commit

---
+++ JSONparsingpractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSONparsingpractice
+//
+//  Created by Student-004 on 12/01/17.
+//  Copyright © 2017 abhijit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JSONparsingpractice/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JSONparsingpractice
+//
+//  Created by Student-004 on 12/01/17.
+//  Copyright © 2017 abhijit. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
